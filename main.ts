36ec3baa228bf235526be70705c8691cb36d899d
elroei19 <119411919+elroei19@users.noreply.github.com>
2023-02-14 12:32:44 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,7 @@
 basic.forever(function () {
-    basic.showArrow(ArrowNames.South)
     Tinybit.CarCtrl(Tinybit.CarState.Car_Run)
-    basic.pause(1000)
-    basic.showArrow(ArrowNames.North)
-    Tinybit.CarCtrl(Tinybit.CarState.Car_Back)
-    basic.pause(1000)
-    basic.showArrow(ArrowNames.East)
-    Tinybit.CarCtrl(Tinybit.CarState.Car_SpinLeft)
-    basic.pause(1000)
-    basic.showArrow(ArrowNames.West)
-    Tinybit.CarCtrl(Tinybit.CarState.Car_SpinRight)
-    basic.pause(1000)
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        # # # # #
-        # # # # #
-        . # . # .
-        `)
-    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
-    basic.pause(1000)
+})
+basic.forever(function () {
+    Tinybit.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.Red))
+    Tinybit.RGB_Car_Big2(0, 0, 0)
 })
